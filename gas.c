1031dbae9c824f79a49142aa2a4cf4dc24fa509f
julie0005 <julie0005@ajou.ac.kr>
2021-06-11 13:05:32 +0900

    gas code changed

--- gas.c
+++ gas.c
@@ -140,117 +140,6 @@ static int GPIOWrite(int pin, int value){
     return(0);
 }
 
-static int
-PWMExport(int pwmnum){
-#define BUFFER_MAX 3
-      char buffer[BUFFER_MAX];
-      int bytes_written;
-      int fd;
-      
-      fd = open("/sys/class/pwm/pwmchip0/unexport", O_WRONLY);
-      if(-1 == fd){
-         fprintf(stderr, "Failed to open in unexport!\n");
-         return (-1);
-      }
-      
-      bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pwmnum);
-      write(fd, buffer, bytes_written);
-      close(fd);
-      
-      sleep(1);
-      fd = open("/sys/class/pwm/pwmchip0/export", O_WRONLY);
-      if(-1 == fd){
-         fprintf(stderr, "Failed to open in export!\n");
-         return(-1);
-      }
-      bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pwmnum);
-      write(fd, buffer, bytes_written);
-      close(fd);
-      sleep(1);
-      return(0);
-}
-
-static int
-PWMEnable(int pwmnum){
-   static const char s_unenable_str[] = "0";
-   static const char s_enable_str[] = "1";
-   #define DIRECTION_M 45
-   char path[DIRECTION_M];
-   int fd;
-   
-   
-   snprintf(path, DIRECTION_M, "/sys/class/pwm/pwmchip0/pwm%d/enable", pwmnum);
-   fd = open(path, O_WRONLY);
-   if(-1 == fd){
-      fprintf(stderr, "Failed to open in enable!\n");
-      return -1;
-   }
-   
-   write(fd, s_unenable_str, strlen(s_unenable_str));
-   close(fd);
-   
-   fd = open(path, O_WRONLY);
-   if(-1 == fd){
-      fprintf(stderr, "Failed to open in enable!\n");
-      return -1;
-   }
-   
-   write(fd, s_enable_str, strlen(s_enable_str));
-   close(fd);
-   return (0);
-   
-}
-
-static int
-PWMWritePeriod(int pwmnum, int value){
-   char s_values_str[VALUE_MAX];
-   char path[VALUE_MAX];
-   int fd, byte;
-   
-   //
-   snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm%d/period", pwmnum);
-   fd = open(path, O_WRONLY);
-   if(-1 == fd){
-      fprintf(stderr, "Failed to open in period!\n");
-      return(-1);
-   }
-   
-   byte = snprintf(s_values_str, 10, "%d", value);
-   if(-1 == write(fd, s_values_str, byte)){
-      fprintf(stderr, "Failed to write value in period!\n");
-      close(fd);
-      return(-1);
-   }
-   
-   close(fd);
-   return(0);
-}
-
-static int
-PWMWriteDutyCycle(int pwmnum, int value){
-   char path[VALUE_MAX];
-   char s_values_str[VALUE_MAX];
-   int fd, byte;
-   
-   snprintf(path, VALUE_MAX, "/sys/class/pwm/pwmchip0/pwm%d/duty_cycle", pwmnum);
-   fd = open(path, O_WRONLY);
-   
-   if(-1 == fd){
-      fprintf(stderr, "Failed to open in duty_cycle!\n");
-      return(-1);
-   }
-   
-   byte = snprintf(s_values_str, 10, "%d", value);
-   
-   if(-1 == write(fd, s_values_str, byte)){
-      fprintf(stderr, "Failed to write value! in duty_cycle\n");
-      close(fd);
-      return(-1);
-   }
-   
-   close(fd);
-   return(0);
-}
 
 
 static int prepare(int fd) {
@@ -307,10 +196,6 @@ int *but=&b;
 
 void *gas_thd(){
    int light=0;
-    PWMExport(0);
-    PWMWritePeriod(0,20000000);
-    PWMWriteDutyCycle(0,0);
-    PWMEnable(0);
     
     int fd=open(DEVICE, O_RDWR);
     if(fd<=0){
@@ -324,7 +209,6 @@ void *gas_thd(){
         light=readadc(fd,0);
         g=light;
         printf("%d\n", light);
-        PWMWriteDutyCycle(0, light*7500);
         usleep(400000);
     }
     close(fd);
@@ -498,5 +382,4 @@ int main(int argc, char *argv[]){
    pthread_join(p_thread[3], (void **)&status);
     
 }
-
 
